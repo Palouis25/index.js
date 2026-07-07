0d7fd2bc587eaa9d3e7c141f1bc2dbb8d4579133
Palouis25 <palouisjarju146@gmail.com>
2026-07-07 11:36:28 +0000

    Initial commit

---
+++ OneDrive/Desktop/Me/index.js
@@ -0,0 +1,20 @@
+
+console.log("Hello, World!");
+console.log("My name is Pa Louie");
+console.log("I am learning JavaScript");
+
+let name = "Pa Louie";
+let age = 25;
+let city = "Banjul";
+let isStudent = true;
+let favoriteFood = "Pizza";
+let hobby = "Playing football and coding";
+
+console.log(`My name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`I live in ${city}.`);
+console.log(`Am I a student? ${isStudent}`);
+console.log(`My favorite food is ${favoriteFood}.`);
+console.log(`My hobby is ${hobby}.`);
+
+console.log(`My name is ${name}, I am ${age} years old, I live in ${city}, am I a student? ${isStudent}, my favorite food is ${favoriteFood}, and my hobby is ${hobby}.`);
